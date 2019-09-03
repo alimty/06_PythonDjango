7d1ada3c0f2d283584662348d45f9cf01b252968
ali Mengütay <alozicyber@gmail.com>
2019-09-03 22:19:06 +0200

    twitt

--- DjangoTraining/Django-Ders/djangoDers/note/views.py
+++ DjangoTraining/Django-Ders/djangoDers/note/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Person
 
-# Create your views here.
+
+def index(request):
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    title = request.POST.get('title')
+    note = request.POST.get('note')
+    result = Person.objects.create(
+      username=username,
+      title=title,
+      note=note, 
+    )
+    if result:
+      print('Kayit basarili')
+    else:
+      print('Kayit yapilmadi')
+
+  return render(request, 'index.html')
+
+
+def show_notes(request):
+    context = dict()
+    context ['notes'] = Person.objects.all().order_by('-pk')
+    return render(request, 'notes.html', context)

98abc936f3b763c851c3d02125f987e47fa8e142
ali Mengütay <alozicyber@gmail.com>
2019-09-04 22:20:21 +0200

    js

---
+++ JS-Ders/script.js
@@ -0,0 +1,14 @@
+
+
+(function(x) { return (function(y){console.log(x);})(2)})(1);
+
+
+let array1 = "ali".split(''); 
+let array2 = array1.reverse(); 
+let array3 = "anil".split('');  
+
+array2.push(array3);   
+
+// console çıktısı ; 
+// array 1 : uzunluğu ? , sonuncu = a,l,i 
+// array 1 : uzunluğu ? , sonuncu = l,i,n,a
